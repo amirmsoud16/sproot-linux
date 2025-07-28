1036aa8a1ade66335828459363ff0b78e4c73f0c
amirmsoud16 <amir.iran.019@gmail.com>
2025-07-28 15:49:39 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -79,7 +79,6 @@ print_menu() {
     echo -e "\n${WHITE}Available Options:${NC}"
     echo -e "${BLUE}1.${NC} System Check & Preparation"
     echo -e "${BLUE}2.${NC} Install Ubuntu (Chroot/Proot)"
-    echo -e "${BLUE}3.${NC} Fix Chroot Links"
     echo -e "${BLUE}4.${NC} Remove Ubuntu"
     echo -e "${BLUE}5.${NC} Access Ubuntu"
     echo -e "${BLUE}6.${NC} Exit"
